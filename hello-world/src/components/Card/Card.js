faa80c78606f91b196fb9881b8e498fb40f1e0ce
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2024-10-31 23:32:23 -0400

    Update Card.js

--- hello-world/src/components/Card/Card.js
+++ hello-world/src/components/Card/Card.js
@@ -1,10 +1,11 @@
 import Cardstyles from './card.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faStar} from '@fortawesome/free-solid-svg-icons';
+
 import { useState } from 'react';
 import usePokemonApi from '@/hooks/getPokemon';
 import Link from 'next/link';
+import { faL } from '@fortawesome/free-solid-svg-icons';
 export default function Card({name, types, sprite, id,favoriteStyle, onClick, toggleRedirect}) {
+    const [ischecked, setIsChecked] = useState(false);
     const pokeData = usePokemonApi();
  
    
@@ -12,7 +13,7 @@ export default function Card({name, types, sprite, id,favoriteStyle, onClick, to
     return (
         
         <div id={id} className={Cardstyles.card} >
-            <FontAwesomeIcon onClick={onClick} style={{stroke: 'black', strokeWidth: '50px', color:favoriteStyle}} icon={faStar} />
+            <input type="checkbox" onClick={onClick} defaultChecked={ischecked} onChange={() => setIsChecked(!ischecked)}  />
             <h2>{pokeData.capitalizeFirstLetter(name) || "Unknown"}</h2>
             <p>Types:{pokeData.capitalizeFirstLetter(types) || "Unknown"} </p>
             <img  src={sprite || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwpbKiUFba7go6lXL6Ryu4B3F4cC1qPpbvqw&s"} alt={name} className={Cardstyles.sprite}/>
